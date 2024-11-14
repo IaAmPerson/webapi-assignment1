b159c480a90287f9ce30d41bc0578f2fe3a1f745
IaAmPerson <ylt012321@gmail.com>
2024-11-14 21:57:25 +0800

    2

--- Yash_EGL301.js
+++ Yash_EGL301.js
@@ -1,10 +1,10 @@
-// Database array
+//Database array
 let rooms = [];
 
 
-// Add new room type function
+//Add new room type
 function addRoomType(roomTypeName) {
-    // Checking if room type already exists
+    //Check if room type already exists
     let roomTypeExists = rooms.some(room => room.roomTypeName === roomTypeName);
     if (!roomTypeExists) {
         rooms.push({ roomTypeName, rooms: [] }); // Add new room type
@@ -14,18 +14,18 @@ function addRoomType(roomTypeName) {
     }
 }
 
-// Function to remove an existing room type
+//Remove room type
 function removeRoomType(roomTypeName) {
-    // Filter out the room type from the array
+    //Filter out the room type from the array
     rooms = rooms.filter(room => room.roomTypeName !== roomTypeName);
     console.log(`Room type '${roomTypeName}' removed successfully.`);
 }
 
-// Function to add a room number to a specific room type
+//Add a room number to a specific room type
 function addRoomNumber(roomTypeName, roomNumber, level) {
     let roomType = rooms.find(room => room.roomTypeName === roomTypeName);
     if (roomType) {
-        // Add the room number with details
+        //Add room number with details
         roomType.rooms.push({ roomNumber, level });
         console.log(`Room number '${roomNumber}' added to room type '${roomTypeName}'.`);
     } else {
@@ -33,11 +33,11 @@ function addRoomNumber(roomTypeName, roomNumber, level) {
     }
 }
 
-// Function to remove a room number from a room type
+//Remove a room number from a room type
 function removeRoomNumber(roomTypeName, roomNumber) {
     let roomType = rooms.find(room => room.roomTypeName === roomTypeName);
     if (roomType) {
-        // Filter out the room number from the room type
+        //Filter out the room number from the room type
         roomType.rooms = roomType.rooms.filter(room => room.roomNumber !== roomNumber);
         console.log(`Room number '${roomNumber}' removed from room type '${roomTypeName}'.`);
     } else {
@@ -45,12 +45,12 @@ function removeRoomNumber(roomTypeName, roomNumber) {
     }
 }
 
-// Function to get the entire list of rooms
+//Get all rooms
 function getAllRooms() {
     return rooms;
 }
 
-// Function to get all room numbers under a specific room type
+//Get all room numbers under a specific room type
 function getRoomsInType(roomTypeName) {
     let roomType = rooms.find(room => room.roomTypeName === roomTypeName);
     if (roomType) {
@@ -61,7 +61,7 @@ function getRoomsInType(roomTypeName) {
     }
 }
 
-// Function to search for a room by room number
+//Search for room by room number
 function searchRoom(roomNumber) {
     for (let roomType of rooms) {
         let room = roomType.rooms.find(r => r.roomNumber === roomNumber);
@@ -73,16 +73,16 @@ function searchRoom(roomNumber) {
     return null;
 }
 
-// Function to get room details by room number
+//Get room details by room number
 function getRoomDetails(roomNumber) {
     return this.searchRoom(roomNumber);
 }
 
-// Function to update room details
+//Update room details
 function updateRoomDetails(roomNumber, newLevel) {
     let room = this.searchRoom(roomNumber);
     if (room) {
-        room.level = newLevel; // Update the level
+        room.level = newLevel; //Update room level
         console.log(`Room number '${roomNumber}' updated to level '${newLevel}'.`);
     } else {
         console.log(`Room number '${roomNumber}' not found for update.`);
